134b31a2dbd54dc15a0080974cfd215a999920ce
Lenovo <faganzeynalli>
2025-03-02 01:26:17 +0400

    npm run build

---
+++ web/static/vite_dist/useConfirmAndDelete-D8fwf-Am.js
@@ -0,0 +1,1 @@
+import{f as c,d as u}from"./usePlayerStore-DrK7fhNm.js";import{u as r}from"./useDialog-D7Bru7kZ.js";function D(o,e){const{confirmDelete:s}=r(),{notifySuccess:i}=c(),{axios:a}=u();return{doDelete:async n=>{const{value:f}=await s({title:o});if(f){const{data:t}=await a.delete(n);i(t.message),typeof e=="function"&&e(t)}}}}export{D as u};
--- web/static/vite_dist/useConfirmAndDelete-OgT0B0nj.js
+++
@@ -1,1 +0,0 @@
-import{f as c,d as u}from"./usePlayerStore-DS4JWrVc.js";import{u as r}from"./useDialog-CxBg2-5w.js";function D(o,e){const{confirmDelete:s}=r(),{notifySuccess:i}=c(),{axios:a}=u();return{doDelete:async n=>{const{value:f}=await s({title:o});if(f){const{data:t}=await a.delete(n);i(t.message),typeof e=="function"&&e(t)}}}}export{D as u};
